c4adf57491d08301b381d30347f5e08ef92cb145
Francois Le <francoisle@outlook.com>
2019-03-23 21:45:09 +0100

    Camera is working properly

--- App.js
+++ App.js
@@ -5,13 +5,49 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+import {RNCamera} from 'react-native-camera';
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+    this.takePicture = this.takePicture.bind(this);
+  }
+
+  componentDidMount() {
+    console.log("Component did mount !");
+    console.log("Camera", this.camera);
+  }
+
+  async takePicture() {
+    if (this.camera) {
+      const options = {quality: 0.5, base64: true};
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          captureAudio={false}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.auto}
+          permissionDialogTitle={'Permission to use camera'}
+          permissionDialogMessage={'We need your permission to use your camera phone'}
+        />
+        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{fontSize: 14}}> Take a photo ! </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -20,8 +56,21 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
   }
 });
